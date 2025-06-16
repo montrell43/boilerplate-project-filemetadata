d95b4a5b269ef0e2a88bfe27a7073d722072b587
montrell43 <tmerriweather@persevernow.org>
2025-06-16 12:40:12 -0500

    commit

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
-    size: req.filesize
+    size: req.file.size
   })
 })
 
-
+//hslhbbsiadsbad
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
